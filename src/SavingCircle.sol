3839a3b61e41508d29523e4339dc7421ccf931e8
Poglavar Svemira <svemir@nftmagicians.xyz>
2025-11-22 12:24:30 -0300

    Reverse the inheritance

--- src/SavingCircle.sol
+++ src/SavingCircle.sol
@@ -2,45 +2,39 @@
 pragma solidity ^0.8.23;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {SavingCircleSeat} from "./SavingCircleSeat.sol";
 
-contract SavingCircle is SavingCircleSeat {
+contract SavingCircle {
     address[] public registeredUsers;
     address[] public usersWhoDidNotWin;
 
     // from original user to current owner
     mapping(address => address) public addressOwner;
-    mapping(address => uint256) public seatTokenId;
-    mapping(uint256 => address) public originalUserOfToken;
 
     // round numer to user to auction size of user
-    mapping(uint256 => mapping(address => uint256)) roundAuctionSize;
+    mapping(uint => mapping(address => uint)) roundAuctionSize;
 
-    IERC20 public immutable installmentToken;
-    IERC20 public immutable protocolToken;
+    IERC20 immutable public installmentToken;
+    IERC20 immutable public protocolToken;
 
-    uint256 public immutable installmentSize;
-    uint256 public immutable protocolTokenRewardPerInstallment;
-    uint256 public immutable numRounds;
-    uint256 public immutable startTime;
-    uint256 public immutable timePerRound;
-    uint256 public immutable numUsers;
+    uint immutable public installmentSize;
+    uint immutable public protocolTokenRewardPerInstallment;
+    uint immutable public numRounds;
+    uint immutable public startTime;
+    uint immutable public timePerRound;
+    uint immutable public numUsers;
 
-    uint256 public nextRoundToPay = 0;
-
-    error UnknownSeat(uint256 tokenId);
-    error UnauthorizedSeatCaller();
+    uint public nextRoundToPay = 0;
 
     constructor(
         address _installmentToken,
         address _protocolToken,
-        uint256 _installmentSize,
-        uint256 _protocolTokenRewardPerInstallment,
-        uint256 _numRounds,
-        uint256 _startTime,
-        uint256 _timePerRound,
-        uint256 _numUsers
-    ) SavingCircleSeat() {
+        uint _installmentSize,
+        uint _protocolTokenRewardPerInstallment,
+        uint _numRounds,
+        uint _startTime,
+        uint _timePerRound,
+        uint _numUsers)
+    {
         installmentToken = IERC20(_installmentToken);
         protocolToken = IERC20(_protocolToken);
         installmentSize = _installmentSize;
@@ -52,23 +46,20 @@ contract SavingCircle is SavingCircleSeat {
     }
 
     event UserRegister(address a);
-
     function register() public {
         require(addressOwner[msg.sender] == address(0), "already registered");
         require(registeredUsers.length < numUsers, "circle is full");
         require(startTime > block.timestamp, "too late to join");
 
-        uint256 tokenId = _mintSeat(msg.sender);
-        seatTokenId[msg.sender] = tokenId;
+        addressOwner[msg.sender] = msg.sender;
         registeredUsers.push(msg.sender);
         usersWhoDidNotWin.push(msg.sender);
 
         emit UserRegister(msg.sender);
     }
 
-    event UserDepositRound(uint256 round, uint256 auctionSize, address originalUser);
-
-    function depositRound(uint256 round, uint256 auctionSize, address originalUser) public {
+    event UserDepositRound(uint round, uint auctionSize, address originalUser);
+    function depositRound(uint round, uint auctionSize, address originalUser) public {
         require(addressOwner[originalUser] == msg.sender, "not owner");
         require(roundAuctionSize[round][originalUser] == 0, "already paid");
         require(auctionSize > 0, "auction size 0 not allowed");
@@ -86,42 +77,41 @@ contract SavingCircle is SavingCircleSeat {
         emit UserDepositRound(round, auctionSize, originalUser);
     }
 
-    function roundDeadline(uint256 round) public view returns (uint256) {
+    function roundDeadline(uint round) public view returns(uint) {
         return startTime + timePerRound * round;
     }
 
-    function nextRound() public view returns (uint256) {
-        if (block.timestamp < startTime) return 0;
+    function nextRound() public view returns(uint) {
+        if(block.timestamp < startTime) return 0;
 
         return 1 + ((block.timestamp - startTime) / timePerRound);
     }
 
-    event RaffleWin(uint256 round, uint256 seed, address winner);
-
-    function raffle(uint256 round, uint256 seed) internal {
+    event RaffleWin(uint round, uint seed, address winner);
+    function raffle(uint round, uint seed) internal {
         require(round == nextRoundToPay, "previous rounds were not settled yet");
         require(roundDeadline(round) <= block.timestamp, "too early to pay the round");
 
         // select the winner
 
-        uint256 totalAuctionForRound = 0;
-        for (uint256 i = 0; i < usersWhoDidNotWin.length; i++) {
+        uint totalAuctionForRound = 0;
+        for(uint i = 0 ; i < usersWhoDidNotWin.length ; i++) {
             totalAuctionForRound += roundAuctionSize[round][usersWhoDidNotWin[i]];
         }
 
-        uint256 winThreshold = seed % totalAuctionForRound;
-        uint256 weightSoFar = 0;
-        uint256 winnerIndex = 0;
-        for (winnerIndex = 0; winnerIndex < usersWhoDidNotWin.length; winnerIndex++) {
+        uint winThreshold = seed % totalAuctionForRound;
+        uint weightSoFar = 0;
+        uint winnerIndex = 0;
+        for(winnerIndex = 0 ; winnerIndex < usersWhoDidNotWin.length ; winnerIndex++) {
             weightSoFar += roundAuctionSize[round][usersWhoDidNotWin[winnerIndex]];
-            if (weightSoFar >= winThreshold) break;
+            if(weightSoFar >= winThreshold) break;
         }
 
         // pay minimum between balance and installment * num rounds
-        uint256 rewardSize = installmentSize * numRounds;
-        uint256 availBalance = installmentToken.balanceOf(address(this));
+        uint rewardSize = installmentSize * numRounds;
+        uint availBalance = installmentToken.balanceOf(address(this));
 
-        if (availBalance < rewardSize) rewardSize = availBalance;
+        if(availBalance < rewardSize) rewardSize = availBalance;
         address winner = addressOwner[usersWhoDidNotWin[winnerIndex]];
 
         require(installmentToken.transfer(winner, rewardSize), "reward payment failed");
@@ -130,35 +120,13 @@ contract SavingCircle is SavingCircleSeat {
         usersWhoDidNotWin[winnerIndex] = usersWhoDidNotWin[usersWhoDidNotWin.length - 1];
         usersWhoDidNotWin.pop();
 
-        emit RaffleWin(round, seed, winner);
+        emit RaffleWin(round, seed, winner);        
     }
 
-    function isCurrent(address user) public view returns (bool) {
-        if (nextRound() == 0) return true;
+    function isCurrent(address user) public view returns(bool) {
+        if(nextRound() == 0) return true;
 
-        return roundAuctionSize[nextRound() - 1][user] > 0;
-    }
-
-    function updateAddressOwner(uint256 tokenId, address from, address to) external {
-        if (msg.sender != address(this)) revert UnauthorizedSeatCaller();
-        _setSeatOwnership(tokenId, from, to);
-    }
-
-    function _onSeatTransfer(address from, address to, uint256 tokenId) internal override {
-        _setSeatOwnership(tokenId, from, to);
-    }
-
-    function _setSeatOwnership(uint256 tokenId, address from, address to) internal {
-        require(to != address(0), "invalid new owner");
-
-        address original = originalUserOfToken[tokenId];
-        if (original == address(0)) {
-            if (from != address(0)) revert UnknownSeat(tokenId);
-            originalUserOfToken[tokenId] = to;
-            original = to;
-        }
-
-        addressOwner[original] = to;
+        return roundAuctionSize[nextRound() - 1][user] > 0;        
     }
 }
 
--- src/SavingCircleSeat.sol
+++ src/SavingCircleSeat.sol
@@ -3,35 +3,49 @@
 pragma solidity ^0.8.23;
 
 import {ERC721URIStorage, ERC721} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import {SavingCircle} from "./SavingCircle.sol";
 
-abstract contract SavingCircleSeat is ERC721URIStorage {
+contract SavingCircleSeat is SavingCircle, ERC721URIStorage {
     uint256 private _nextTokenId;
     mapping(uint256 => address) private _acceptedRecipient;
+    mapping(address => uint256) public seatTokenId;
 
     error NotAuthorized(address operator, uint256 tokenId);
     error RecipientNotAccepted(uint256 tokenId, address expected);
     error DirectTransferNotAllowed();
 
-    constructor() ERC721("SavingCircleSeat", "SCST") {}
-
     event SeatMinted(uint256 indexed tokenId, address indexed to);
-
-    function _mintSeat(address to) internal returns (uint256 tokenId) {
-        if (to == address(0)) revert ERC721InvalidReceiver(address(0));
-        tokenId = _nextTokenId++;
-        _safeMint(to, tokenId);
-        emit SeatMinted(tokenId, to);
-    }
-
     event TransferAccepted(uint256 indexed tokenId, address indexed by);
+    event TransferExecuted(uint256 indexed tokenId, address indexed from, address indexed to);
+
+    constructor(
+        address _installmentToken,
+        address _protocolToken,
+        uint256 _installmentSize,
+        uint256 _protocolTokenRewardPerInstallment,
+        uint256 _numRounds,
+        uint256 _startTime,
+        uint256 _timePerRound,
+        uint256 _numUsers
+    )
+        SavingCircle(
+            _installmentToken,
+            _protocolToken,
+            _installmentSize,
+            _protocolTokenRewardPerInstallment,
+            _numRounds,
+            _startTime,
+            _timePerRound,
+            _numUsers
+        )
+        ERC721("SavingCircleSeat", "SCST")
+    {}
 
     function acceptTransfer(uint256 tokenId) external {
         ownerOf(tokenId); // reverts if token does not exist
         _acceptedRecipient[tokenId] = msg.sender;
         emit TransferAccepted(tokenId, msg.sender);
     }
-
-    event TransferExecuted(uint256 indexed tokenId, address indexed from, address indexed to);
 
     function executeTransfer(uint256 tokenId, address to) external {
         _requireAuthorized(msg.sender, tokenId);
@@ -55,9 +69,20 @@ abstract contract SavingCircleSeat is ERC721URIStorage {
     function _update(address to, uint256 tokenId, address auth) internal override returns (address) {
         if (auth != address(0)) revert DirectTransferNotAllowed();
         address from = super._update(to, tokenId, auth);
-        _onSeatTransfer(from, to, tokenId);
+        _setSeatOwnership(tokenId, from, to);
         return from;
     }
 
-    function _onSeatTransfer(address from, address to, uint256 tokenId) internal virtual;
+    function register() public override {
+        super.register();
+        uint256 tokenId = _mintSeat(msg.sender);
+        seatTokenId[msg.sender] = tokenId;
+    }
+
+    function _mintSeat(address to) internal returns (uint256 tokenId) {
+        if (to == address(0)) revert ERC721InvalidReceiver(address(0));
+        tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+        emit SeatMinted(tokenId, to);
+    }
 }
